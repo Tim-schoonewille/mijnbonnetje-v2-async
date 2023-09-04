f65b7e5709166c56ab7a8b189ca7a7ac23453745
Tim Schoonewille <Tim.schoonewille@gmail.com>
2023-09-04 18:47:18 +0200

    Added get, get multi routes to receipt_file with tests

--- backend/project/app/routers/receipt_file.py
+++ backend/project/app/routers/receipt_file.py
@@ -1,3 +1,4 @@
+import os
 from uuid import UUID
 
 
@@ -37,3 +38,55 @@ async def create_receipt_file(
     if receipt_entry_in_db.user_id != user.id:
         raise HTTPException(status_code=403, detail="NOT_YOUR_ENTRY")
     return await handle_receipt_file(db, user, file, entry_id, settings.STATIC_FOLDER)
+
+
+@router.get('/', response_model=list[models.ReceiptFile])
+async def read_multiple_receipt_files(
+    user: VerifiedUser,
+    params: ParametersDepends,
+    db: GetDB,
+):
+    """ Return multiple receipt files, depending on given parameters
+    requires verified user token
+    """
+    return await crud.receipt_file.get_multi(db, user, **params)
+
+
+@router.get('/{receipt_file_id}', response_model=models.ReceiptFile)
+async def read_specific_receipt_file(
+    receipt_file_id: int | UUID,
+    user: VerifiedUser,
+    db: GetDB,
+):
+    """ Returns specific receipt file from database, requires verified user token
+
+        Raises:\n
+            403: {detail: "NOT_YOUR_RECEIPT_FILE"}
+            404: {detail: "RECEIPT_FILE_NOT_FOUND"}
+    """
+    receipt_file_in_db = await crud.receipt_file.get(db, receipt_file_id)
+    if receipt_file_in_db is None:
+        raise HTTPException(status_code=404, detail="RECEIPT_FILE_NOT_FOUND")
+    if receipt_file_in_db.user_id != user.id and not user.sudo:
+        raise HTTPException(status_code=403, detail="NOT_YOUR_RECEIPT_FILE")
+    return receipt_file_in_db
+
+
+@router.delete('/{receipt_file_id}', response_model=dict[str, str])
+async def delete_receipt_file(
+    receipt_file_id: int | UUID,
+    user: VerifiedUser,
+    settings: GetSettings,
+    db: GetDB,
+):
+    """ deeletes a specific receipt file from database, requires verified user token
+
+        Raises:\n
+            403: {detail: "NOT_YOUR_RECEIPT_FILE"}
+            404: {detail: "RECEIPT_FILE_NOT_FOUND"}
+    """
+    receipt_file_in_db = await crud.receipt_file.get(db, receipt_file_id)
+    if receipt_file_in_db is None:
+        raise HTTPException(status_code=404, detail="RECEIPT_FILE_NOT_FOUND")
+    if receipt_file_in_db.user_id != user.id and not user.sudo:
+        raise HTTPException(status_code=403, detail="NOT_YOUR_RECEIPT_FILE")
--- backend/project/tests/mijnbonnetje/test_012_receipt_file.py
+++ backend/project/tests/mijnbonnetje/test_012_receipt_file.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 
 from app import models
@@ -15,6 +16,7 @@ def test_create_receipt_file(client):
         "/receipt-entry", json={**receipt_entry_schema.model_dump(exclude_unset=True)}
     )
     assert response.status_code == 201
+
     response = client.post(
         "/receipt-file/",
         files={"file": open(test_receipt_file_path, "rb")},
@@ -25,3 +27,39 @@ def test_create_receipt_file(client):
     assert data['fileName'] == 'test_receipt.jpg'
     assert data['fileType'] == 'image/jpeg'
     assert data['fileSize'] == 186030
+
+
+def test_read_receipt_file(client):
+    response = client.get('/receipt-file/1')
+    data = response.json()
+    entry_id = data['receiptEntryId']
+    file_name = data['fileName']
+    file_path = str(data['filePath'])
+    assert response.status_code == 200
+    assert data['createdAt'] is not None
+    assert data['updatedAt'] is None
+    assert data['userId'] == 10
+    assert entry_id == 1
+    assert file_name == 'test_receipt.jpg'
+    assert data['fileType'] == 'image/jpeg'
+    assert data['fileSize'] == 186030
+    assert file_path.startswith(f'./public-test/{entry_id}/')
+    assert file_path.endswith(file_name)
+
+
+def test_read_multiple_files(client):
+    response = client.get('/receipt-file/')
+    data = response.json()
+    assert response.status_code == 200
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+def test_delete_receipt_file(client):
+    receipt_file_on_disk = client.get('/receipt-file/1').json()['filePath']
+    assert os.path.exists(receipt_file_on_disk)
+    response = client.delete('receipt-file/1')
+    data = response.json()
+    assert response.status_code == 200
+    assert data['message'] == 'RECEIPT_FILE_DELETED'
+    assert os.path.exists(receipt_file_on_disk) is False
